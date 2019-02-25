81359516d627cbf53506799e85a0f8f3bcb3dba9
cr57.davidj <cr57.davidj@enfrausaph.com>
2019-02-25 22:32:43 +0800

    add initial files and error handler

---
+++ backend/handlers/error.js
@@ -0,0 +1,7 @@
+module.exports = function errorHandler(err, request, response, next){
+    return response.status(err.status || 500).json({
+        error: {
+            message: err.message || 'Oops! Something went wrong.'
+        }
+    })
+}
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express       = require('express');
+const app           = express();
+const CORS          = require('cors');
+const bodyParser    = require('body-parser');
+const errorHandler  = require('./handlers/error');
+
+const PORT      = 8081;
+
+app.use(CORS());
+app.use(bodyParser.json());
+
+app.use(function(req,res, next){
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`APP IS NOW RUNNING ON PORT ${PORT}`));
